ef323419f7a352af12eb1bf3c274fef7986454e2
jligeiro <ligeirojaoa03@gmail.com>
2026-07-07 22:01:09 +0100

    version 1.1.02

--- src/sistema.c
+++ src/sistema.c
@@ -259,7 +259,7 @@ void cadastrar_cliente(Sistema *sistema) {
     }
 
     cliente->idade = ler_inteiro("Idade: ");
-    if (cliente->idade <= 17){
+    if (cliente->idade <= 17) {
         printf("Erro: idade invalida.\n");
         free(cliente);
         return;
